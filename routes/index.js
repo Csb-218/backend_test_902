0162b7950e5fcf0052813d8ce1eb3dcf8a3e80d3
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-02-09 13:37:02 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Backend Test 902' });
+  res.render('index', { title: 'Customizable Caching API' });
 });
 
 module.exports = router;
